ad5e10d831c0c720e0903dae0ca75bdb9d455fad
andervyd <andervyd@gmail.com>
2020-08-15 20:20:54 -0700

    connection maven mysql

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,7 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("test...");
+		System.out.println("add class...");
+		System.out.println("add maven mysql connection...");
 	}
 }
